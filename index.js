73ef51401780d838e2f671f7c99c0e30aa6933af
Gabriel Samaniego <selfdev.gs@gmail.com>
2018-11-08 10:51:40 -0800

    dynamic port

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // define a server
 const server = require("./api/server");
-const port = 9000;
+const port = process.env.PORT || 9000;
 // run the server
 server.listen(port, () => console.log(`\nAPI running on port ${port}\n`));
